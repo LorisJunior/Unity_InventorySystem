c78aec7f4c4915b36fd9887d392995a3e0ccb28e
Lourival <em-lourival@outlook.com>
2024-04-08 15:19:03 -0300

    Scene done

---
+++ Assets/Scripts/AnimationHash.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class AnimationHash
+{
+    public static int moveX = Animator.StringToHash("Move X");
+    public static int moveY = Animator.StringToHash("Move Y");
+    public static int speed = Animator.StringToHash("Speed");
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,19 +10,32 @@ public class PlayerController : MonoBehaviour
     public float Speed = 5f;
 
     private Vector2 move;
+    private Vector2 lookDirection;
     private Rigidbody2D rigidbody2d;
+    private Animator animator;
 
     // Start is called before the first frame update
     private void Start()
     {
         moveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     private void Update()
     {
         move = moveAction.ReadValue<Vector2>();
+
+        if (!Mathf.Approximately(move.x, 0f) || !Mathf.Approximately(move.y, 0f))
+        {
+            lookDirection = move;
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat(AnimationHash.moveX, lookDirection.x);
+        animator.SetFloat(AnimationHash.moveY, lookDirection.y);
+        animator.SetFloat(AnimationHash.speed, move.magnitude);
     }
 
     private void FixedUpdate()
